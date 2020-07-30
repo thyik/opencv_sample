76c927737eedd643de1d72609caa512234c7a769
yikth <netyth@gmail.com>
2020-07-30 11:53:55 +0800

    test eye detection

--- webcam_facedetection.py
+++ webcam_facedetection.py
@@ -3,6 +3,7 @@ import sys
 
 cascPath = sys.argv[1]
 faceCascade = cv2.CascadeClassifier(cascPath)
+eyeCascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
 
 video_capture = cv2.VideoCapture(0)
 
@@ -21,8 +22,23 @@ while True:
     )
 
     # Draw a rectangle around the faces
+    i=0
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # faceROI = gray(faces[i])
+        # eyes = eyeCascade.detectMultiScale(
+        #     faceROI)
+        # i += 1
+        # # Draw a rectangle around the eyes
+        # for (x1, y1, w1, h1) in faces:
+        #     cv2.rectangle(frame, (x1, y1), (x1+w1, y1+h1), (0, 0, 255), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+
+        eyes = eyeCascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
